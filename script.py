d4905ecaadd8d6f74dcf9af054b16a74c4321b2f
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-05-24 18:09:44 -0400

    Testing

--- script.py
+++ script.py
@@ -1,14 +1,28 @@
 import os
 from git import Repo
+import getpass
+
 
 ## Paths where the bubble directories should be saved.
 bubblePath = os.getcwd() + '/bubbles'
 bubblePrivatePath = os.getcwd() + '/bubbles/bubbles_private'
 
+## Prompt User for username / password.
+username = raw_input('Github Username:')
+password = getpass.getpass('Github Password:')
+
+
+## Authentication String.
+auth = 'https://' + username + ':' + password
+
 ## Both bubble urls.
-bubble_url = 'https://github.com/bubblegroup/bubble'
-bubble_private_url = 'https://github.com/jphaas/bubble_private'
+bubble_url = auth + '@github.com/bubblegroup/bubble'
+bubble_private_url = auth + '@github.com/jphaas/bubble_private'
+
+
 
 ## Create the repos.
-Repo.clone_from(bubble_url, bubblePath)
-Repo.clone_from(bubble_private_url, bubblePrivatePath)
+clonedRepo = Repo.clone_from(bubble_url, bubblePath)
+clonedRepo2 = Repo.clone_from(bubble_private_url, bubblePrivatePath)
+
+print clonedRepo
