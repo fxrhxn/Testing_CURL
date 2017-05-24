91dd2516f4d3ca6e1996ba62d229c59b80adab13
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-05-24 17:07:56 -0400

    Testing

--- script.py
+++ script.py
@@ -1,8 +1,18 @@
 import os
-print os.getcwd()
 from git import Repo
 
-git_url = 'https://github.com/fxrhxn/testing-raspberrypi'
+bubblePath = os.getcwd() + '/bubbles'
+
+bubblePrivatePath = os.getcwd() + '/bubbles/bubbles_private'
+
+print bubblePath
+print bubblePrivatePath
+
+
+bubble_url = 'https://github.com/fxrhxn/testing-raspberrypi'
+bubble_private_url = 'https://github.com/fxrhxn/testing-raspberrypi'
 
 repo_dir = 'Random_S'
-Repo.clone_from(git_url, "TestingMore")
+
+Repo.clone_from(bubble_url, bubblePath)
+Repo.clone_from(bubble_private_url, bubblePrivatePath)
