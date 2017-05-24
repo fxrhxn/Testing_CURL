1ab2fea3244e55d716c462c08126be888dd4f00b
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-05-24 16:32:01 -0400

    Updated the script

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print 'Testing script.'
+import os
+print os.getcwd()
+from git import Repo
+
+git_url = 'https://github.com/fxrhxn/testing-raspberrypi'
+
+repo_dir = 'Random_S'
+Repo.clone_from(git_url, "/Users/farhanrahman/Desktop/Testing")
